7a123bca582bd4a3e65bc4433b4263ad0f7373c8
nadaali <nadaeloksh55@gmail.com>
2025-05-15 00:09:33 +0300

    lastt

--- automata_practical/questions_files/question2_cyk_algorithm.py
+++ automata_practical/questions_files/question2_cyk_algorithm.py
@@ -4,17 +4,17 @@ def cyk_algorithm(cfg, string):
     variables = cfg['variables']
     table = [[set() for _ in range(n)] for _ in range(n)]
 
-    # ملء الطبقة الأولى بناءً على الإنتاجات الطرفية
+    
     for i in range(n):
         for var in variables:
             if [string[i]] in cfg['productions'].get(var, []):
                 table[i][i].add(var)
 
-    # ملء باقي الجدول
-    for l in range(2, n + 1):  # length of substring
-        for i in range(n - l + 1):  # start index
-            j = i + l - 1  # end index
-            for k in range(i, j):  # split point
+    
+    for l in range(2, n + 1):  
+        for i in range(n - l + 1):  
+            j = i + l - 1  
+            for k in range(i, j):  
                 for var in variables:
                     for prod in cfg['productions'].get(var, []):
                         if len(prod) == 2:
@@ -22,5 +22,5 @@ def cyk_algorithm(cfg, string):
                             if B in table[i][k] and C in table[k + 1][j]:
                                 table[i][j].add(var)
 
-    # هل المتغير الابتدائي في الركن الأعلى الأيسر؟
+    
     return cfg['start'] in table[0][n - 1]
